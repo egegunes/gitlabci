5a974b22bc5abc53ed06291a1c66b46c6663d36f
Ege Güneş <egegunes@gmail.com>
2019-12-15 21:20:55 +0300

    Move config logic to root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,26 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringP("token", "t", "SECRET", "Your Gitlab API Access Token")
+	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
+}
+
+func initConfig() {
+	viper.SetConfigName(".gitlab")
+	viper.AddConfigPath("$HOME/.config/gitlabci")
+	viper.AddConfigPath("$HOME")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "couldn't read config file: %v", err)
+		os.Exit(1)
+	}
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "gitlabci [command]",
